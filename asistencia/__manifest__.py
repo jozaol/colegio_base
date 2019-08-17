8fb6b5a8146cbb229b663ae709a34a02540aaa0f
Jose Zambrano <jozaol@gmail.com>
2019-08-17 16:55:41 -0500

    [UPD] dando action a boton alumno

--- asistencia/__manifest__.py
+++ asistencia/__manifest__.py
@@ -4,6 +4,7 @@
     'author': 'Jose Zambrano',
     'depends': ['base','hr_attendance'],
     'data': [
+        'views/web_assets_backend_template.xml',
     ],
     'qweb': [
         'static/src/xml/attendance.xml',
