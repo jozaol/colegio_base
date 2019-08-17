f0217963ddd6770fd44f7e51b8683b94735eeca9
Jose Zambrano <jozaol@gmail.com>
2019-08-17 15:00:21 -0500

    [UPD] boton asistencia

---
+++ asistencia/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Asistencia',
+    'version': '1.0',
+    'author': 'Jose Zambrano',
+    'depends': ['base','hr_attendance'],
+    'data': [
+    ],
+    'qweb': [
+        # 'static/xml/asistencias.xml',
+    ],
+}
