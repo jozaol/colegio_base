ec58d464890ac90b00e238eccd5603a3ee5ca648
Andre Zambrano <andre.zambrano.cueva@gmail.com>
2019-10-12 09:10:01 -0500

    [DELL ALLL]

--- colegio_base_1/models/subject.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class OpSubject(models.Model):
-    _inherit = "op.subject"
-    _description = "Subject plus"
-
-    subject_padre_id=fields.Many2one('op.subject','Curso Padre')
-
--- registro_notas/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import notas_trimestre
-from . import notas_unidad
-from . import notas_anyaca
-from . import notas_asignaturas
-from . import notas_criterios_libreta
-from . import notas_meses_pension
-from . import notas_nivel
